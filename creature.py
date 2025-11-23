07dae09a7e83b0689b4d3b611b2a25a633ee7ced
relayable <relayable28@gmail.com>
2025-11-23 11:14:49 -0600

    added bestiary navigation and display

--- creature.py
+++ creature.py
@@ -1,6 +1,28 @@
 class Creature():
-    def __init__(self, name, size, type, rarity):
+    def __init__(self, name, size, type, rarity, region):
         self.name = name
         self.size = size #tiny, small, medium, large, giant
         self.type = type #winged, burrower, quick-footed, strong 
-        self.rarity = rarity
+        self.rarity = rarity #common, uncommon, rare
+        self.region = region
+
+    def page_display(self):
+        print(rf"""
+
+                    __________________   __________________
+                .-/|                  \ /                  |\-.
+                ||||{self.name.center(19)}|{(self.size + ", " + self.type).center(19)}||||
+                ||||     --==*==--     |{"creature".center(19)}||||
+                ||||{"Found in the".center(19)}|{"~~*~~".center(19)}||||
+                ||||{self.region.upper().center(19)}|                   ||||
+                ||||                   |                   ||||
+                ||||                   |{self.rarity.upper().center(19)}||||
+                ||||                   |{"--==*==--".center(19)}||||
+                ||||                   |                   ||||
+                ||||                   |                   ||||
+                ||||                   |                   ||||
+                ||||__________________ | __________________||||
+                ||/===================\|/===================\||
+                `--------------------~___~-------------------''
+
+        """)
--- story.py
+++ story.py
@@ -7,42 +7,86 @@ def main():
 
     success = meadow.find_creature()
     if success:
-        print(meadow.random_creature().name)
+        found = meadow.random_creature()
+        print(found.name)
+        bestiary.append(found)
     else:
         print("You didn't find anything!")
 
+    success = meadow.find_creature()
+    if success:
+        found = meadow.random_creature()
+        print(found.name)
+        bestiary.append(found)
+    else:
+        print("You didn't find anything!")
 
+    choice = input("Open bestiary? Y/N > ").strip().upper()
+    if choice == "Y":
+        navigate_bestiary(bestiary)
+    elif choice == "N":
+        print("ok then")
+    else:
+        print("Invalid input!")
+
+
+
+def navigate_bestiary(bestiary):
+        page = 0
+        toggle = ""
+        while toggle != "X":
+            bestiary[page].page_display()
+
+            print(f"Viewing page {page + 1} of {len(bestiary)}")
+
+            toggle = input("View next page (D), view previous page (A), or close bestiary (X) > ").strip().upper()
+
+            if toggle == "A" and page == 0:
+                print("You are already on the first page!")
+            elif toggle == "A":
+                page -= 1
+            elif toggle == "D" and page == len(bestiary) - 1:
+                print("You are already on the last page!")
+            elif toggle == "D":
+                page += 1
+            elif toggle == "X":
+                continue
+            else:
+                print("Invalid input!")
+
+    
 
 def setup():
     meadow_animals = []
 
     #MEADOW
     #common
-    meadow_animals.append(Creature("Mouse", "tiny", "burrower", "common"))
-    meadow_animals.append(Creature("Hare", "small", "quick-footed", "common"))
+    meadow_animals.append(Creature("Mouse", "tiny", "burrower", "common", "meadow"))
+    meadow_animals.append(Creature("Hare", "small", "quick-footed", "common", "meadow"))
 
     #uncommon
-    meadow_animals.append(Creature("Snake", "small", "burrower", "uncommon"))
-    meadow_animals.append(Creature("Grasshopper", "tiny", "winged", "uncommon"))
+    meadow_animals.append(Creature("Snake", "small", "burrower", "uncommon", "meadow"))
+    meadow_animals.append(Creature("Grasshopper", "tiny", "winged", "uncommon", "meadow"))
 
     #rare
-    meadow_animals.append(Creature("Hawk", "medium", "winged", "rare"))
+    meadow_animals.append(Creature("Hawk", "medium", "winged", "rare", "meadow"))
 
     meadow = Region("Meadow", meadow_animals, .8) 
 
 
     glade_animals = []
+    
     #GLADE
     #common
-    glade_animals.append(Creature("Songbird", "small", "winged", "common"))
-    glade_animals.append(Creature("Squirrel", "small", "quick-footed", "common"))
+    glade_animals.append(Creature("Songbird", "small", "winged", "common", "glade"))
+    glade_animals.append(Creature("Squirrel", "small", "quick-footed", "common", "glade"))
 
     #uncommon
-    glade_animals.append(Creature("Fox", "small", "quick-footed", "uncommon"))
-    glade_animals.append(Creature("Faun", "medium", "quick-footed", "uncommon"))
+    glade_animals.append(Creature("Fox", "small", "quick-footed", "uncommon", "glade"))
+    glade_animals.append(Creature("Faun", "medium", "quick-footed", "uncommon", "glade"))
 
     #rare
-    glade_animals.append(Creature("Stag", "medium", "strong", "rare"))
+    glade_animals.append(Creature("Stag", "medium", "strong", "rare", "glade"))
 
     glade = Region("Glade", glade_animals, .6) 
 
